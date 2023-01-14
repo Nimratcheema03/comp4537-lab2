33d191d7e5dc118e7a3b797094ebc2bd197e2e3b
Nimratcheema03 <nimratcheema03@gmail.com>
2023-01-13 18:01:09 -0800

    hosted webpage

--- backend/index.js
+++ backend/index.js
@@ -33,6 +33,3 @@ const PORT = process.env.PORT  || 3000;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
-
-url = "https://chatbot-bsh7.onrender.com"
-backend = process.env.url

83008ced19dda9bbc2c5fb1986bf0a5d9c7f055e
Nimratcheema03 <nimratcheema03@gmail.com>
2023-01-13 18:56:43 -0800

    trying to fix

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.post('/chatbot', (req, res) => {
 	}
 });
 
-app.get("/chatbot",(req, res)=>{
+app.get("/",(req, res)=>{
 	res.sendFile("/frontend/index.html")
 } )
 
--- frontend/script.js
+++ frontend/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 	  const message = $('#chatbot-input').val();
 	  $.ajax({
 		type: 'POST',
-		url: 'https://chatbot-td3a.onrender.com',
+		url: 'https://chatbot-td3a.onrender.com/chatbot',
 		// url: 'http://localhost:3000/chatbot',
 		data: { message: message },
 		success: function (response) {
